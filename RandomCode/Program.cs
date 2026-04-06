92381580de8c3529f648396805f80e5c6f0014c6
Michał Urban <micha@DESKTOP-81JDJ0Q>
2026-04-06 10:31:52 +0200

    Add project files.

---
+++ RandomCode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RandomCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
