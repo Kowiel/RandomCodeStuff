af37d6ace947345cd62781d037badb06b55f15ac
Michał Urban <micha@DESKTOP-81JDJ0Q>
2026-04-06 22:57:58 +0200

    Random WArmup

--- RandomCode/Program.cs
+++ RandomCode/Program.cs
@@ -1,10 +1,40 @@
 ﻿namespace RandomCode
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string []strs = { "bat", "bag", "bank", "band" };
+
+            var awnser = LongestCommonPrefix(strs);
+            Console.WriteLine(awnser);
         }
+
+        #region Stuff
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length == 0)
+                return "";
+
+            string prefix = "";
+
+            for (int i = 0;i < strs[0].Length;i++)
+            {
+                for  (int j = 1;j < strs.Length;j++)
+                {
+                    if (i >= strs[j].Length || strs[j][i] != strs[0][i])
+                        return prefix;
+                }
+
+                prefix = prefix + strs[0][i];
+            }
+            return prefix;
+
+        }
+
+
+        #endregion
+
     }
 }
