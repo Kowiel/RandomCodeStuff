0d5405f17a555de1381ee310ab727bc4fc1fcb24
Michał Urban <micha@DESKTOP-81JDJ0Q>
2026-04-07 21:20:55 +0200

    more wamup

--- RandomCode/Program.cs
+++ RandomCode/Program.cs
@@ -1,12 +1,15 @@
-﻿namespace RandomCode
+﻿using System.Diagnostics.Metrics;
+
+namespace RandomCode
 {
     public class Program
     {
         static void Main(string[] args)
         {
             string []strs = { "bat", "bag", "bank", "band" };
+            int []nums = { 1, 2, 3, 4 , 2};
 
-            var awnser = LongestCommonPrefix(strs);
+            var awnser = hasDuplicate(nums);
             Console.WriteLine(awnser);
         }
 
@@ -31,6 +34,59 @@
             }
             return prefix;
 
+        }
+
+        public static bool hasDuplicate(int[] nums)
+        {
+            var MainDictionary = new Dictionary<int,int>();
+            foreach (int num in nums)
+            {
+                if (!MainDictionary.ContainsKey(num))
+                {
+                    MainDictionary.Add(num, num);
+                }
+                else
+                    return true;
+            }
+            return false;
+
+            // optional 
+/* 
+ *             var Dictionary = new Dictionary<int, int>();
+foreach (int num in nums)
+{
+if(!Dictionary.TryAdd(num, 0))
+{
+    return true;
+}
+}
+return false;*/
+
+}
+
+        public bool IsAnagram(string s, string t)
+        {
+            if (s == null || t == null)
+                return false;
+
+            var DictionaryOne = new Dictionary<char,int>();
+            var DictionaryTwo = new Dictionary<char,int>();
+            foreach(char c in s)
+            {
+                if (DictionaryOne.ContainsKey(c))
+                    DictionaryOne[c] +=1;
+                else
+                    DictionaryOne.TryAdd(c, 1);
+            }
+            foreach (char c in t)
+            {
+                if (DictionaryTwo.ContainsKey(c))
+                    DictionaryTwo[c] += 1;
+                else
+                    DictionaryTwo.TryAdd(c, 1);
+            }
+            bool areEqual = DictionaryOne.OrderBy(kv => kv.Key).SequenceEqual(DictionaryTwo.OrderBy(kv => kv.Key));
+            return areEqual;
         }
 
 
